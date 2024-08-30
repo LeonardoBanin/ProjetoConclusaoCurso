2d26ccdf26a70347b243fc7cd3f7d71e120dc9a6
Felipe <fs7269820@gmail.com>
2024-08-30 13:38:04 -0300

    Update script.js

--- FlexFood-Site/tela-pagamentoaprovado/script.js
+++ FlexFood-Site/tela-pagamentoaprovado/script.js
@@ -5,5 +5,5 @@ function navigateToPage() {
 
 function goToHomePage() {
     // Remove o alerta e apenas redireciona para a página inicial
-    window.location.href = "home.html"; // Substitua "home.html" pelo caminho da sua página inicial
+    window.location.href = "http://127.0.0.1:5500/FlexFood-Site/tela-home/index.html"; // Substitua "home.html" pelo caminho da sua página inicial
 }
